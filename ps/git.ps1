afe1a8008c73ccf7ad0e742c181ac1c5f37dce33
arqtiq <gratien.vernier@gmail.com>
2020-08-30 14:57:19 +0200

    working posh

--- ps/git.ps1
+++ ps/git.ps1
@@ -21,11 +21,24 @@ function write_git_status {
         "TF" { $IC_GIT_STATUS_AHEAD + $match.groups['ahead'].value }
         "FT" { $IC_GIT_STATUS_BEHIND + $match.groups['behind'].value }
     }
-    if ($changes) {
-        
-    }
 
     Write-Host "$or $bri $br $s" -NoNewline -Fore $CO_PR_GIT_FORE -Back $CO_PR_GIT_BACK
+
+    if ($changes) {
+        $A = $B = $C = $E = $F = $G = 0
+        for ($i = 1; $i -lt $sta.Length; $i++) {
+            $l = $sta[$i].SubString(0, 2)
+            if ($l[0] -eq "M")      { $B++ }
+            elseif ($l[0] -eq "R")  { $B++ }
+            elseif ($l[0] -eq "A")  { $A++ }
+            elseif ($l[0] -eq "D")  { $C++ }
+            if ($l[1] -eq "M")      { $F++ }
+            elseif ($l[1] -eq "R")  { $F++ }
+            elseif ($l[1] -eq "D")  { $G++ }
+            if ($l -eq "??")        { $E++ }
+        }
+        Write-Host " +$A ~$B -$C | +$E ~$F -$G" -NoNewline -Fore $CO_PR_GIT_FORE -Back $CO_PR_GIT_BACK
+    }
 }
 
 function gs { git status -sb }
