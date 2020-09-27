8d93aa8833157345eb8fd5891a72713720150813
arqtiq <gratien.vernier@gmail.com>
2020-09-27 15:19:05 +0200

    check theme exists before dl

--- ps/themes.ps1
+++ ps/themes.ps1
@@ -23,6 +23,10 @@ $tagsMap = @{
     "Cursor Color" = "cursorColor";
 }
 
+function Browse-Themes {
+    start $themes_url
+}
+
 function Use-Theme {
     param (
         [string] $theme,
@@ -45,65 +49,7 @@ function Use-Theme {
         return
     }
 
-    # get requested theme link
-    $request = Invoke-WebRequest -Uri $themes_url -UseBasicParsing
-    $xmlLink = $null
-    foreach ($l in $request.Links) {
-        if ($l.outerHTML -match "<strong>(.*?)<\/strong>") {
-            $n = $Matches.1
-            if ($n -eq $theme) {
-                $xmlLink = $l.Href
-            }
-        }
-    }
-    if ($xmlLink -eq $null) {
-        Write-Host "Can't find requested theme" -Fore Red
-        return
-    }
-
-    # get theme xml
-    $request = Invoke-WebRequest -Uri $xmlLink -UseBasicParsing
-    $xml = [System.Xml.Linq.XDocument]::Parse($request.content)
-
-    # parse xml & convert to json
-    $themeObj = @{ name = $theme }
-    $currentColor = $null
-    $color = @{}
-    [array]$colors = $xml.Root.Element("dict").Elements()
-
-    for($i = 0; $i -lt $colors.Length; $i++) {
-        $k = $colors[$i]
-        $tag = $k.name
-        if($tag -eq "key") {
-            if($currentColor -ne $null) {
-                $themeObj[$tagsMap[$currentColor]] =
-                "#" + $color.Red.ToString() + $color.Green.ToString() + $color.Blue.ToString()
-            }
-            # skip color if not used
-            if (!$tagsMap.ContainsKey($k.Value)) {
-                $i++
-                continue
-            }
-            $currentColor = $k.Value
-        }
-        else {
-            [array]$vals = $k.Elements()
-            for($j = 0; $j -lt $vals.Length; $j+=2) {
-                $_k = $vals[$j]
-                if (($_k.Value.Contains("Alpha")) -or ($_k.Value.Contains("Space"))) {
-                    continue
-                }
-                $comp = $_k.Value.Replace("Component", "").Trim()
-                $cval = (($vals[$j+1].Value -as [float]) * 255) -as [int]
-                $color[$comp] = "{0:X2}" -f $cval
-            }
-        }
-    }
-    
-    $themeJson = ($themeObj | ConvertTo-Json)
-
-    # patch settings
-    # strip comments
+    # load settings
     $settingsJson = @()
     foreach($l in Get-Content -Path $settings) {
         if (!($l.Trim().StartsWith("//"))) {
@@ -112,16 +58,75 @@ function Use-Theme {
     }
     $settingsJson = ($settingsJson -join "`n") | ConvertFrom-Json
 
+    # search if theme already exists
     $existing = -1
     for ($k = 0; $k -lt $settingsJson.schemes.Count; $k++) {
         if ($settingsJson.schemes[$k].name -eq $theme) {
             $existing = $k
         }
     }
+
     if ($existing -lt 0) {
+        # get requested theme link
+        $request = Invoke-WebRequest -Uri $themes_url -UseBasicParsing
+        $xmlLink = $null
+        foreach ($l in $request.Links) {
+            if ($l.outerHTML -match "<strong>(.*?)<\/strong>") {
+                $n = $Matches.1
+                if ($n -eq $theme) {
+                    $xmlLink = $l.Href
+                }
+            }
+        }
+        if ($xmlLink -eq $null) {
+            Write-Host "Can't find requested theme" -Fore Red
+            return
+        }
+
+        # get theme xml
+        $request = Invoke-WebRequest -Uri $xmlLink -UseBasicParsing
+        $xml = [System.Xml.Linq.XDocument]::Parse($request.content)
+
+        # parse xml & convert to json
+        $themeObj = @{ name = $theme }
+        $currentColor = $null
+        $color = @{}
+        [array]$colors = $xml.Root.Element("dict").Elements()
+
+        for($i = 0; $i -lt $colors.Length; $i++) {
+            $k = $colors[$i]
+            $tag = $k.name
+            if($tag -eq "key") {
+                if($currentColor -ne $null) {
+                    $themeObj[$tagsMap[$currentColor]] =
+                    "#" + $color.Red.ToString() + $color.Green.ToString() + $color.Blue.ToString()
+                }
+                # skip color if not used
+                if (!$tagsMap.ContainsKey($k.Value)) {
+                    $i++
+                    continue
+                }
+                $currentColor = $k.Value
+            }
+            else {
+                [array]$vals = $k.Elements()
+                for($j = 0; $j -lt $vals.Length; $j+=2) {
+                    $_k = $vals[$j]
+                    if (($_k.Value.Contains("Alpha")) -or ($_k.Value.Contains("Space"))) {
+                        continue
+                    }
+                    $comp = $_k.Value.Replace("Component", "").Trim()
+                    $cval = (($vals[$j+1].Value -as [float]) * 255) -as [int]
+                    $color[$comp] = "{0:X2}" -f $cval
+                }
+            }
+        }
+        
+        # add theme
         $settingsJson.schemes += $themeObj
     }
 
+    # set as default theme
     if ($d) {
         if ($settingsJson.profiles.defaults.PSObject.Properties.Match("colorScheme").Count) {
             $settingsJson.profiles.defaults.colorScheme = $theme
@@ -131,11 +136,11 @@ function Use-Theme {
         }
     }
 
+    # set as profile theme
+    if ($p) {
+        $currentProfile = $host.UI.RawUI.WindowTitle
+    }
+
+    # save back settings
     $settingsJson | ConvertTo-Json | Out-File $settings -Encoding ASCII
-
-    #$settingsJson.profiles.list
-
-    #if ($p) {
-      #  $currentProfile = $host.UI.RawUI.WindowTitle
-    #}
 }
